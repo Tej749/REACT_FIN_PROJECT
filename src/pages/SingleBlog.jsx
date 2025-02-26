27741a6543b634866ef2860ee99ecb89331281b8
Tej749 <jargha261975@gmail.com>
2025-02-26 12:34:34 +0545

    update delete API using axios method

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,19 +1,33 @@
 import axios from "axios";
 import Navbar from "../components/Navbar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function SingleBlog() {
 
-  const data = useParams() 
-  console.log(data)
-  const fetchSingleBlog = () =>{
-    axios.get("http://localhost:3000/blog")
-  }
+  const {id} = useParams() 
 
+  const [blog, setBlog] = useState({})
+  
+  console.log(id)
+  const fetchSingleBlog = async () =>{
+    const response = await axios.get("http://localhost:3000/blog/" + id)
+    setBlog(response.data.data) // response hold data in object form: key & value pair 
+    //console.log(response.data.data)
+  }
+ 
   useEffect(() =>{
     fetchSingleBlog() 
   }, [])
+
+  const deleteBlog = async () =>{
+    const response = await axios.delete("http://localhost:3000/blog/" + id)
+    console.log("delete blog execute vayo hai...")
+  }
+
+  const editBlog = () =>{
+    console.log("Edit block executed hudai xa...")
+  }
   return (
     <>
       <Navbar />
@@ -22,26 +36,28 @@ function SingleBlog() {
         <div className="flex flex-col md:flex-row -mx-4">
             <div className="md:flex-1 px-4">
                 <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-                    <img className="w-full h-full object-cover" src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg" alt="Product Image" />
+                    <img className="w-full h-full object-cover" src={`http://localhost:3000/` + blog.image} alt="Product Image" />
                 </div>
                 <div className="flex -mx-2 mb-4">
                     <div className="w-1/2 px-2">
-                        <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit</button>
+                        <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700" onClick={editBlog}>Edit</button>
                     </div>
                     <div className="w-1/2 px-2">
-                        <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Delete</button>
+                        <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600" onClick={deleteBlog}>Delete</button>
                     </div>
                 </div>
             </div>
             <div className="md:flex-1 px-4">
-                <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">Product Name</h2>
+                <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">{blog.game}</h2>
+                <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">{blog.add}</h2>
+
                 <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-                    ante justo. Integer euismod libero id mauris malesuada tincidunt.
+                   Webpage design using express on backend & ReactJS on frontend (UI) and MongoDB (No SQL Database)
                 </p>
                
                 <div>
-                    <span className="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
+                  <h2>Player Name </h2>
+                    <span className="font-bold text-gray-700 dark:text-gray-300">{blog.player}</span>
                     <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
                         sed ante justo. Integer euismod libero id mauris malesuada tincidunt. Vivamus commodo nulla ut
